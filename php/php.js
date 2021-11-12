907cdd4bd5f082cd35aa2bb89f08c44764ff3ca5
misaka10843 <69132853+misaka10843@users.noreply.github.com>
2021-11-12 09:15:19 +0800

    Update php.js

--- php/php.js
+++ php/php.js
@@ -1,1 +1,115 @@
-console.log("%c \n PHP世界上最好的语言！", "background: url(https://i.loli.net/2021/11/12/9nOKfwxphs8EAUc.png)  no-repeat 0 0")
+console.log("\
+                                             it  ;                           :                      \n\
+                                           j .                              f;.                     \n\
+                                          ;: .. :                            LL                     \n\
+                                         t,.  .:                            .ft                     \n\
+                                         .,. f                             :jt       ftiLjt.        \n\
+                                         :;.f      t,   .ifi              j,;f     ifi;i;::;f       \n\
+                                         t:i    .Li . . .::. :,;tfLLfftfi,.j:      j .   ij.:.t.    \n\
+                                  Li..   ,fj:  f ...  ..   .::::::::.:::.f.       .    .j:.j.:.f    \n\
+                                f ..:. . :i::,L. :;;;:..,..   .::.:,;jf,              ;t .L:;..     \n\
+                               .. .: ... .:::::,t,,;i,;,:.:,:. i,                          L... .   \n\
+                              ..  : ..... .,,. .:fi,  .,,:::,;,..f                         j..: f   \n\
+                             .   ...    .  ,.,...,;j .   :,,..,,, .,                      L.....,   \n\
+                            G . .: ....     :.   .if;;;;;, ,,,..,,:.f                    L .:.:,,   \n\
+                           ...; ::..: ..... , ;,:,:j;;,,;;,,:,,:.:,,.,;                f.. :.:.:,   \n\
+                           f  i.::..::.... .:.::,.:L:;,,: .::,,,,::,:  ;t           f;  ...,. ,:f   \n\
+                           i.;;.:,..:,.,::. .: ...:,j.,,,,. .::::,,::,.   .ifLLff;.  . ..::: ,:..   \n\
+                          ,:.i:. : :.,::::...: t .i:  f;::,.. ::,::,,,,.. ...        ..:::.::::f    \n\
+                          f :.:i,i.,.,;ttt:..: ;,;;:      :jL  .::,;,.:.:,:...........:i:,,:::.     \n\
+                          L.i  ;.:;,.,.i,,:..:.:,,,:.         f .::,:,,,.,,,,,:....:,,,: ,:,::i     \n\
+                          G:t.  ; i,:::.,,,:::.:;;i:.          L  :,.:,,,,,:,,,,,,::::,:.:::,,  ,f  \n\
+                          f:f ..::.:t,j..;,t::.:;ji.            L ,::..,,,i.,:::::::...:.:::ft::,jG:\n\
+                          ..t DWDG,jit;GD;t:.,::;,,j             j Li:.:,::,i:,,:... .:.:L.         \n\
+                           :i ;DKj.:, iWKEGfft:;,,,L ft          , . t,......,,i;:::i;.f            \n\
+                           Lt,;fft  ; .EDGG..,.t,;,Lj            j f   jt  .  ..:,.,f;              \n\
+                           Ltt Lf:....  DL,...::  tL             ;t       :jffLLLi.                 \n\
+                           iD;.. ,,.... ,.....t,  .t           ::                                   \n\
+                           ::: ..  ..   . ..., :i.                                                  \n\
+                           , .  ............ ; t..f                                                 \n\
+                           j   ..............;..:i                                                  \n\
+                            DG ..............G. i:L                                                 \n\
+                    ; ,,     ,j . .;..  ...::L   ;:j                                                \n\
+                    ;..,      G;:.... . .,,:::Gj   Lf                                               \n\
+    jL               ,ff,i      tL   .:;,,:.::fGGGG, Gi                                             \n\
+     :          ;LfG     ftiLfjjjjDLLLLt,::...DGLLLLf ;,i                                           \n\
+         t;  Li   ,,:  iLi;i;;::..;DGGG,,:....DGLLLLG   ff                                          \n\
+   :jLGDGDGLf;  ,  i;Lt,:.,,;j,..iGGGGGi:::..DDGLLLLLi   fj                                         \n\
+ jfGGDDDDDDGGGf    fi;iii ..::..iGGGGLGD::.:jGDLLLLLLj    Lf                                        \n\
+    .:iDDGGL;.     i  .  ;.....iLGDGGGGGL::LLGLLLLLLGt     i                                        \n\
+  GD  jGGt:::.. . .   .  ;...itfGf..DGL ,LL  LLLLLLLL:     t                                        \n\
+  :    :  .i,:.. ,..; ..t. ;;j:;GGD,tG      DLLLLLLGDj     t                                        \n\
+           LL;,i::.,: i,.i;jjL.EGfj:..     jDLLLLLLGfG                                              \n\
+          j;;.:,,,: ,i.;;j,i:::::::.. :    D.:LLLLGG L                                              \n\
+        ,i.: ::.. ,;.t,L;,:tjjLtf;,:. .;  : ...GLGGG.t                                              \n\
+        t:. ;..ii.,,tti:.L,,::LG,DG:.: .ii .. .:LGG. ;                                              \n\
+       f:. ...:;,iGij:Lji L,::.;.:Gf::. .,  . .:,GD:.,L                                             \n\
+       fj,,,,iLj.          :.::.,..;:::...:.....DED .:j,                                            \n\
+                           L:::...:,t.::. .:...,GD . ;j t                                           \n\
+                            :::::;.f::::: .: . DD... ii   i                                         \n\
+                            ;:::.:.f;i:::. ...fG .:  ,,   :                                         \n\
+                            f::::.,  t:.:....t::::.                                                 \n\
+                             ,:::.i   L...:.i,,:...        i                                        \n\
+                             f.::f     t:.:,,.:            it                                       \n\
+                              GfL       .,i.:..           :iiG                                      \n\
+                                        i:....            t;t;:                                     \n\
+                                        :,....            tii;i                                     \n\
+                                        :,.....  ..      f;;;;,i                                    \n\
+                                        ..:  ..:..      j,,,,,,f                                    \n\
+                                        ,.,:           t;,,,,,;;                                    \n\
+                                        ii..:         j;;,;,,;;,;                                   \n\
+                                         Gtt... .   i,,,;;;,,,f,G                                   \n\
+                                         Ljtttttiii,,,,;;;,,,,,;j                                   \n\
+                                         ii;;i;;tj,;,;,;;;,,,,,,i                                   \n\
+                                          ;;;i;,j;;,,,,;;;;,,,;,;tf                                 \n\
+                                          i;;;;,,fj;;,;;;,,,,,,;;ti                                 \n\
+                                          L;;;;;;;ti;,;;;,,,,,;,;tiG    .                           \n\
+                                          ;;i;;,,;;i;;i;,,,,,,,;,ji;iLfGLG                          \n\
+                                           i;;;,,;;i;;i;,,,,,,;i;jiijfjfGDL                         \n\
+                                           Li;;;,,,,;;i;,,,,,;,,,fi;iLfjDDt                         \n\
+                                           j;;;;,,,,iij;,,,,,,;;;tiiittiDDf                         \n\
+                                            i;;;,,,,iij,,,,,,;,;,tt;;;j;LDL.                        \n\
+                                            f;;;,,,,;tt,,,,,,,,;,itii;L;;DL:                        \n\
+                                            t;;;,,,,;j;,,,,,,,,,i,i;i;iLGDL:                        \n\
+                                             t;;,,,,;t;,,,,,,;,,,,i;i;;LGLG,                        \n\
+                                             i;i;,,,;;t,,,,,,,,,,;i;i;;GLLGi                        \n\
+                                              L;;;;,;;tt,,,,,,,,,;i,;;;DLLGi                        \n\
+                                               j;;;,;;t;,;,,,,,,,;;,;;;fGLG;                        \n\
+                                ,LLGL;         :;;;;,;i,i,,,,,,,,,,,;;;iLGj                         \n\
+                             tGjttttttjj        i;i;i;;;,t,,,,,,,,;,;;;;GGL                         \n\
+                   GjjL,   ttttttttttjttjj       j;;;i;,,;i;;,,,,,,,,;;;j.                          \n\
+                  LtttjtLtjttttttttttttttjDLfjfL, j;iiii;;;;,,,,,,,,,;;;i                           \n\
+         LtjG     ftjttjGtjtjttttttttjjftttjtttttL f;iittti;,,,,,,,,,ii;j                           \n\
+         ,fLjjG   fjjjjGtjttttjttjttjjjjttttttttjtij;i;itjttji;;,,,,;i;;t                           \n\
+          GjjjtL. ffjjGjjtjjLtjLEjtjjjjtttttttttjjf ;i;iiiijttjiiii;i;;;:                           \n\
+          ffjtttfifffLjjttjffEELjjjjjjjjtttttttttjjffii;;i;titiiti;i;i;;                            \n\
+         LfLttttttLfLjjjjjjjjjjEDtjjjjjjtttttttttjjfLj;;tii;tiiiiit;ii;t                            \n\
+         DffjtttjtjtjGfjjjjjjjjtjjjjjjjjjjjttttttjjfffGjiiii;iii;iiiii,,LL,                         \n\
+          .ffjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjtttttjjfffffffDi;;;;;iiii;iL:.fi                         \n\
+           LfjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjtttttjtGffffjjtff;;;;iii;;t::..i:                        \n\
+           .jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjttttttGfffjjjjjjtL;;;;;;fj:::.:iGi                       \n\
+            tfftjjjjjjjjjjttjjjjjjjjjjjjjjjjttttjfffjjjjjjjjtj,tjG,  :.: fitf            ,;         \n\
+             :DfjjjtjjjjjtLDjGjjjjjjjjjjjjjjtjtjDffjtjftjjtjjf        f:iiiit:          ,:t         \n\
+            .    ifGGGfLGGGfffLtjjjjjjjjjjjjjjjDGfLjtGLLDGtjjjf       LLiiitij         .:::         \n\
+             .      ;fLfffftjfLfjjjjjjjjjjjtjjDDGELGjDGfELjjjtL        Gjtiii;j        ,::          \n\
+             ;       :LffffjfjjtjjjjjLjjjjjjfEfLLGDLDjLGGGLjjjti         Lti;;j.      ;;;,          \n\
+                      LfffjjjjjjjjjjjjfLLLGGDDLLGGtGLtLLGDtjjjjE         :i,,,jf,    j;;;t ,i       \n\
+            i         LjjjjjjjjjjjtjjjjfjffGDfGEDGjDGLDGftjjjjfGG         ;;,tfjf,f  ji;;;,,i       \n\
+            jj       jjtjjjjjjjjffjjjjjjjffDLGGGjGDjDfLGjttjjjfGLG        :ijfffjDLf   t;;;i        \n\
+              :      fjjjjjfLfLfffjjjjjjjjjLLEfttDGtttjGGjjjjjfGtfG        GfLfjfGGD   ji;;,i       \n\
+               ti    tjjjjjjfffffjjjjjjjjjjjGLjjjtjjjjjLLjjjjffL ;LL        Li;;jLDL   ii;;;,::i    \n\
+                    LjjjjjjjjjffjjjjjjjjjjjjDDjjjjjjjjjtjjjjjffL   GG       ti,,iGGL   tii;;,::::i. \n\
+                   .fjjjjjjjjjjjjjjjjjjjjjjjDDtjjjjjjjjjjjtjffff:   GL     LjjjLiEDj:  iiiittiii;;: \n\
+                 .tftjjjjjjjjjjjjjjjjjjjjjjjjjjtjjjjjGLfffffffff.    Lf.  DjjfjffDLt  jiii:         \n\
+                 LjjtjjjjjftjjjjjjjjjjjjjjjjjjjfjjLG:  ,fffffLLf      ;f; ftjffffLLj   :iii         \n\
+                 GGLjjjjjfffjtjjjjjjjjjjjjjjjjGGGGD      GfffffG        L,jffjffLLL      ti         \n\
+                  DtLjjjjffG;ftjjjjjjjjjjjjjjjDGGG.       GffLfG         GfffLLLLfG,LG              \n\
+                   DjLjjffG   ;GfjjtjjjjjjjjjfGGG;         GfLG,          GLLfLLjDfLG:              \n\
+                    DfffLt         :LjjjjjjjjfGGi          .L.             GGDt:LfjLLj.             \n\
+                      i;             GjjjjjtjLt.                            GLG;GLfL;fDL            \n\
+                                     .fjjjjjfL                                     ,                \n\
+                                      fjjjjjf.                                                      \n\
+                                       LjjjjG                                                       \n\
+                                       LjjjL.                                                       \n\
+                                       ft,                                                          \n\
+PHP世界上最好的语言！（建议将调试窗口宽度扩大一些qwq）")
