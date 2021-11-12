c0a04ff5a0855fd005b813bb6e66ca0de5915495
misaka10843 <69132853+misaka10843@users.noreply.github.com>
2021-11-12 08:56:18 +0800

    Update and rename phpnb.js to php.js

---
+++ php/php.js
@@ -0,0 +1,1 @@
+console.log("%c \n PHP世界上最好的语言！", "background: url(https://i.loli.net/2021/11/12/9nOKfwxphs8EAUc.png)  no-repeat 0 0")
--- php/phpnb.js
+++
@@ -1,1 +0,0 @@
-console.log("%c \n PHP世界上最好的语言！", "background: url(https://i.loli.net/2021/11/12/9nOKfwxphs8EAUc.png) no-repeat center;")
